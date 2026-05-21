86b3687e244c47829bd1e2844b0aa6d182b2d2a8
parmaryuvraj <parmaryuvrajv78@gmail.com>
2026-05-21 13:36:52 +0530

    Update project

--- models/Chat.js
+++ models/Chat.js
@@ -4,7 +4,7 @@ const chatSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: true,
+    required: false,
   },
   title: {
     type: String,
